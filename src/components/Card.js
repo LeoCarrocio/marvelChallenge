99e93e5ff7009ead9be3b6a4f007c2197e884f0e
LeoCarrocio <leocarrocio@gmail.com>
2020-03-25 18:02:42 -0300

    card realizados

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+// import imgCardPrueva from '../utils/img/img_card_hulk.jpg';
+import star from '../utils/img/star_black.png';
 
 
+const CardContainers = styled.div`
+  display: grid;
+  background-image: url(${(props)=> props.bg});
+  background-position: 0px 0px;
+  background-repeat: no-repeat;
+  background-size: contain;
+  opacity: 1;
+ 
+  .favorite{
+    justify-self: end;
+  }
+  .favorite img{
+    height: 30px;
+  }
+  .title{
+    justify-self: start;
+    align-self: end;
+  }
+`
 
-const Card = () =>{
-
+const Card = (props) =>{
+  const {name,img} = props.character;
+  
   return(
-    <div>
-      <h1>
-        SOY Card
-      </h1>
-    </div>
+
+      <CardContainers bg = {img} >
+      <div className="favorite">
+        <img src={star} alt='derir a favoritos'/>
+      </div>
+      <div className="title">
+        {name}
+      </div>
+     </CardContainers>  
   )
-
-
-
 }
 export default Card;
---
+++ src/controller/fech.js
@@ -0,0 +1,24 @@
+import Axios from 'axios';
+import Crypto from 'crypto-js';
+
+const API_KEY = '2dc9ce9f3db383f25c333aa91d066dcf';
+const PRIV_KEY = 'ff5b9b365db19d96f6187e51c4b00217adacf49e';
+const URI = 'https://getway.marvel.com/v1/public/characters/';
+
+export const fetchHero = (id) =>{
+  const ts = new Date().getTime();
+  let hash = Crypto.MD5(ts+ PRIV_KEY + API_KEY);
+  hash = hash.toString(Crypto.enc.Hex);
+
+  return Axios.get(`${URI}${id}?apiKey=${API_KEY}&ts=${ts}&hash=${hash}`).then(res => res.data);
+
+}
+
+export const fetchHeroes = (name) =>{
+  const ts = new Date().getTime();
+  let hash = Crypto.MD5(ts+ PRIV_KEY + API_KEY);
+  hash = hash.toString(Crypto.enc.Hex);
+
+  return Axios.get(`${URI}${name}?apiKey=${API_KEY}&ts=${ts}&hash=${hash}`).then(res => res.data);
+
+}
