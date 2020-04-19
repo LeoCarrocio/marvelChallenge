f3e8a034f25cb9e2cb79d908ea71c1fd6a20b629
LeoCarrocio <leocarrocio@gmail.com>
2020-04-19 08:19:48 -0300

    modificacion de key

--- src/controller/fech.js
+++ src/controller/fech.js
@@ -5,7 +5,10 @@ const limit = 100;
 const modifiedSince = 1990;
 
 
-
+// const API_KEY = '2dc9ce9f3db383f25c333aa91d066dcf';
+// const PRIV_KEY = 'ff5b9b365db19d96f6187e51c4b00217adacf49e';
+const API_KEY = '15167e1dfc990dce5f4fe9cfab0ea160';
+const PRIV_KEY = 'dcce00ab8bb1a145c457b9f1abc07261edfb491d';
 const HERO_FECH = 'https://gateway.marvel.com:443/v1/public/characters/';
 const HEROS_FECH = 'https://gateway.marvel.com:443/v1/public/characters';
    
@@ -26,7 +29,7 @@ export const fetchHero = (id) =>{
 export const fetchHeroes = (name) =>{
   // console.log(name);
   let {ts , hash} = hashApi();
-  return Axios.get(HEROS_FECH,{ params:{ nameStartsWith:name, apikey: API_KEY, ts,hash} })
+  return Axios.get(HEROS_FECH,{ params:{ nameStartsWith:name,orderBy:'-modified',limit:8, apikey: API_KEY, ts,hash} })
 }
 
 export const fetchHeroesRandon = () =>{
